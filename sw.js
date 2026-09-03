2070ea5c6cbe9b14c32e8d397fed5d1a09ea636e
masixz <79066665+masixz@users.noreply.github.com>
2026-09-03 11:43:05 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-const CACHE='study-timer-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r || fetch(e.request))
-  );
-});
+const C='v2';self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
